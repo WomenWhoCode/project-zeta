a2391f5abe30882053c2437a49af18aa79d6fb91
Georgia A <and.georgia.m@gmail.com>
2015-01-06 22:36:07 -0800

    use arel query for tag counts, fix duplicates in tag cloud

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -10,12 +10,40 @@ module TagsHelper
 		{links: links, events: events}
 	end
 
-	def all_tag_counts
-		# so this is how to return an activerecord from a union
-		# the query's not great though, might be able to rewrite w/o the union
-		tag_count_union = Link.tag_counts.union(Event.tag_counts)
-		tags = Tag.arel_table
-		Tag.from(tags.create_table_alias(tag_count_union, :tags))
+	def tag_counts_combined
+		# SQL:
+		# select tags.*, taggings.tags_count as count
+		# from "tags" join (
+	  #			select taggings.tag_id, count(taggings.tag_id) as tags_count
+		#			from "taggings"
+		#			where taggings.taggable_type in ('Link', 'Event')
+		#						and taggings.context = 'tags'
+		#						and (taggings.taggable_id in (select links.id from "links") or taggings.taggable_id in (select events.id from "events"))
+		# 		group by taggings.tag_id
+		# 		having count(taggings.tag_id) > 0
+		# ) as taggings
+		# on taggings.tag_id = tags.id
+
+		links = Link.arel_table
+		events = Event.arel_table
+		# tag stuff doesn't inherit from activerecord
+		taggings = Arel::Table.new('taggings')
+		tags = Arel::Table.new('tags')
+
+		tagging_subselect = taggings.where(taggings[:context].eq('tags'))
+						.where(taggings[:taggable_type].in(['Link', 'Event']))
+						.where(taggings[:taggable_id].in(links.project(links[:id]))
+									 .or(taggings[:taggable_id].in(events.project(events[:id]))))
+						.group(taggings[:tag_id])
+						.having(taggings[:tag_id].count.gt(0))
+						.project(taggings[:tag_id], taggings[:tag_id].count.as('tags_count')).as('taggings')
+
+		tag_join = tags.join(tagging_subselect)
+				.on(taggings[:tag_id].eq(tags[:id]))
+				.project(tags[:id].as('id'), tags[:name].as('name'), taggings[:tags_count].as('count'))
+
+		# if there's a better way to do this Arel::SelectManager -> ActiveRecord thing, I'm dying to hear it
+		Tag.find_by_sql(tag_join.to_sql)
 	end
 
 end
