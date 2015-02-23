573caa671b8f5d007b2b5447a0f521e53cfd30a2
Georgia A <and.georgia.m@gmail.com>
2015-02-22 19:54:47 -0800

    tabs for difficulty levels

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,21 @@ class StaticPagesController < ApplicationController
   end
 
   def tutorials
-    @featured_tutorials = Link.tagged_with('tutorial').is_featured
+    tutorial_levels = %w{beginner intermediate advanced}
+
+    tutorials = Link.tagged_with('tutorial').is_featured
+
+    # noinspection RubyStringKeysInHashInspection
+    @featured_tutorials = {'all' => []}
+
+    tutorial_levels.each do |level|
+      @featured_tutorials[level] = []
+    end
+
+    tutorials.each do |tutorial|
+      @featured_tutorials['all'] << tutorial
+      level = (tutorial_levels & tutorial.tags.map{|tag| tag.to_s}).first
+      @featured_tutorials[level] << tutorial unless level.nil?
+    end
   end
 end
